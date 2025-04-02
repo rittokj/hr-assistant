4d3b3f581f2f861f469d377b50a2a82ed3203014
Rio's <kj.ritto@live.in>
2025-04-02 11:34:52 +0530

    swipe button style --fix

--- components/RnSwipeButton.tsx
+++ components/RnSwipeButton.tsx
@@ -1,47 +1,46 @@
-import React, { useCallback, useState } from "react";
-import { View, Text, Image, useColorScheme } from "react-native";
+import React, { useCallback, useState } from 'react';
+import { View, Text, Image, useColorScheme } from 'react-native';
 
-import SwipeButton from "rn-swipe-button";
+import SwipeButton from 'rn-swipe-button';
 
-import ArrowSimpleIcon from "@/assets/svgs/ArrowSimple";
+import ArrowSimpleIcon from '@/assets/svgs/ArrowSimple';
 
 function RnSwipeButton() {
-  const colorScheme = useColorScheme();
-  return (
-    <View>
-      <SwipeButton
-        railBackgroundColor="#428EFB"
-        railBorderColor="#428EFB"
-        railStyles={{
-          backgroundColor: "#428EFB",
-          borderColor: "#428EFB",
-        }}
-        thumbIconBackgroundColor={
-          colorScheme === "dark" ? "#171717" : "#FFFFFF"
-        }
-        thumbIconBorderColor="#428EFB"
-        thumbIconComponent={() => <ArrowSimpleIcon color="#428EFB" />}
-        titleColor="#fff"
-        titleComponent={() => (
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              flex: 1,
-              width: "100%",
-              paddingLeft: 60,
-              paddingRight: 20,
-            }}
-          >
-            <Text style={{ color: "#fff", fontSize: 14, fontWeight: "600" }}>
-              Swipe right to check in
-            </Text>
-            <Text style={{ color: "#fff", fontSize: 12 }}>09:03 AM</Text>
-          </View>
-        )}
-      />
-    </View>
-  );
+	const colorScheme = useColorScheme();
+	return (
+		<View>
+			<SwipeButton
+				railBackgroundColor={colorScheme === 'dark' ? '#373737' : '#428EFB'}
+				railBorderColor={colorScheme === 'dark' ? '#373737' : '#428EFB'}
+				railStyles={{
+					backgroundColor: colorScheme === 'dark' ? '#171717' : '#428EFB',
+					borderColor: colorScheme === 'dark' ? '#171717' : '#428EFB',
+				}}
+				thumbIconBackgroundColor={
+					colorScheme === 'dark' ? '#171717' : '#FFFFFF'
+				}
+				thumbIconBorderColor={colorScheme === 'dark' ? '#373737' : '#428EFB'}
+				thumbIconComponent={() => <ArrowSimpleIcon color={'#428EFB'} />}
+				titleColor='#fff'
+				titleComponent={() => (
+					<View
+						style={{
+							flexDirection: 'row',
+							justifyContent: 'space-between',
+							flex: 1,
+							width: '100%',
+							paddingLeft: 60,
+							paddingRight: 20,
+						}}>
+						<Text style={{ color: '#fff', fontSize: 14, fontWeight: '600' }}>
+							Swipe right to check in
+						</Text>
+						<Text style={{ color: '#fff', fontSize: 12 }}>09:03 AM</Text>
+					</View>
+				)}
+			/>
+		</View>
+	);
 }
 
 export default RnSwipeButton;
