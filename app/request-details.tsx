2bf556f7d298dedeefc72bbda8c68e27a5ccc77a
Rio's <kj.ritto@live.in>
2025-04-05 14:49:25 +0530

    style --fix

--- app/request-details.tsx
+++ app/request-details.tsx
@@ -1,6 +1,6 @@
 import { ThemedText } from '@/components/ThemedText';
 import moment from 'moment';
-import { useState } from 'react';
+import { useMemo } from 'react';
 import {
 	View,
 	Text,
@@ -14,87 +14,72 @@ import { useLeaves } from './contexts/LeaveContext';
 
 export default function LeaveFormScreen() {
 	const { selectedLeaveRequest } = useLeaves();
-	console.log('selectedLeaveRequest', selectedLeaveRequest);
 	const colorScheme = useColorScheme();
-	const [formData, setFormData] = useState([
-		{
-			id: 'reqNo',
-			label: 'Request No.',
-			value: selectedLeaveRequest.transNo,
-			type: 'static',
-		},
-		{
-			id: 'date',
-			label: 'Date',
-			value: selectedLeaveRequest.transDate,
-			type: 'date',
-		},
-		{
-			id: 'leaveType',
-			label: 'Leave Type',
-			value: selectedLeaveRequest.leaveTypeDTO.leaveTypeName,
-			type: 'static',
-		},
-		{
-			id: 'halfDay',
-			label: 'Half Day',
-			value: selectedLeaveRequest.isHalfDay ? 'Yes' : 'No',
-			type: 'static',
-		},
-		{
-			id: 'fromDate',
-			label: 'From Date',
-			value: selectedLeaveRequest.leaveFromDate,
-			type: 'date',
-		},
-		{
-			id: 'toDate',
-			label: 'To Date',
-			value: selectedLeaveRequest.leaveToDate,
-			type: 'date',
-		},
-		{
-			id: 'leaveDays',
-			label: 'Leave Days',
-			value: selectedLeaveRequest.leaveDays,
-			type: 'static',
-		},
-		{ id: 'reason', label: 'Reason', value: 'Medical', type: 'static' },
-		{
-			id: 'attachment',
-			label: 'Attach Document',
-			value: '',
-			type: 'attachment',
-		},
-	]);
+
+	const formData = useMemo(
+		() => [
+			{
+				id: 'reqNo',
+				label: 'Request No.',
+				value: selectedLeaveRequest.transNo,
+				type: 'static',
+			},
+			{
+				id: 'date',
+				label: 'Date',
+				value: selectedLeaveRequest.transDate,
+				type: 'date',
+			},
+			{
+				id: 'leaveType',
+				label: 'Leave Type',
+				value: selectedLeaveRequest.leaveTypeDTO.leaveTypeName,
+				type: 'static',
+			},
+			{
+				id: 'halfDay',
+				label: 'Half Day',
+				value: selectedLeaveRequest.isHalfDay ? 'Yes' : 'No',
+				type: 'static',
+			},
+			{
+				id: 'fromDate',
+				label: 'From Date',
+				value: selectedLeaveRequest.leaveFromDate,
+				type: 'date',
+			},
+			{
+				id: 'toDate',
+				label: 'To Date',
+				value: selectedLeaveRequest.leaveToDate,
+				type: 'date',
+			},
+			{
+				id: 'leaveDays',
+				label: 'Leave Days',
+				value: selectedLeaveRequest.leaveDays,
+				type: 'static',
+			},
+			{ id: 'reason', label: 'Reason', value: 'Medical', type: 'static' },
+			{
+				id: 'attachment',
+				label: 'Attach Document',
+				value: '',
+				type: 'attachment',
+			},
+		],
+		[selectedLeaveRequest.transNo]
+	);
 
 	const renderTypes = (item) => {
-		switch (item.type) {
-			case 'date':
-				return (
-					<View>
-						<ThemedText style={{ fontSize: 14, textAlign: 'right' }}>
-							{moment(item.value).format('DD MMM YYYY')}
-						</ThemedText>
-					</View>
-				);
-			case 'static':
-				return (
-					<View>
-						<ThemedText style={{ fontSize: 14, textAlign: 'right' }}>
-							{item.value}
-						</ThemedText>
-					</View>
-				);
-			default:
-				return (
-					<View>
-						<ThemedText style={{ fontSize: 14, textAlign: 'right' }}>
-							{item.value}
-						</ThemedText>
-					</View>
-				);
+		if (item.type === 'date') {
+			return (
+				<ThemedText style={styles.text}>
+					{moment(item.value).format('DD MMM YYYY')}
+				</ThemedText>
+			);
 		}
+		return <ThemedText style={styles.text}>{item.value}</ThemedText>;
 	};
 
 	return (
@@ -109,38 +94,19 @@ export default function LeaveFormScreen() {
 					{formData.map((item) => (
 						<View
 							key={item.id}
-							style={{
-								flexDirection: 'row',
-								marginHorizontal: 20,
-								marginTop: 20,
-								paddingBottom: 15,
-								justifyContent: 'space-between',
-								borderBottomColor: '#ECE9F2',
-								borderBottomWidth: 1,
-							}}>
+							style={styles.container}>
 							<View>
 								<ThemedText style={{ fontSize: 14, textAlign: 'left' }}>
 									{item.label}
 								</ThemedText>
 							</View>
-							{renderTypes(item)}
+							<View>{renderTypes(item)}</View>
 						</View>
 					))}
 
 					<View>
-						<TouchableOpacity
-							style={{
-								margin: 20,
-								marginTop: 30,
-								padding: 15,
-								backgroundColor: '#007AFF',
-								justifyContent: 'center',
-								alignItems: 'center',
-								borderRadius: 10,
-							}}>
-							<Text style={{ color: '#FFF', fontWeight: '600', fontSize: 16 }}>
-								Cancel leave
-							</Text>
+						<TouchableOpacity style={styles.cancelButton}>
+							<Text style={styles.cancelButtonText}>Cancel leave</Text>
 						</TouchableOpacity>
 					</View>
 				</ScrollView>
@@ -150,5 +116,28 @@ export default function LeaveFormScreen() {
 }
 
 const styles = StyleSheet.create({
-	container: {},
+	container: {
+		flexDirection: 'row',
+		marginHorizontal: 20,
+		marginTop: 20,
+		paddingBottom: 15,
+		justifyContent: 'space-between',
+		borderBottomColor: '#ECE9F2',
+		borderBottomWidth: 1,
+	},
+	cancelButton: {
+		margin: 20,
+		marginTop: 30,
+		padding: 15,
+		backgroundColor: '#007AFF',
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderRadius: 10,
+	},
+	cancelButtonText: {
+		color: '#FFF',
+		fontWeight: '600',
+		fontSize: 16,
+	},
+	text: { fontSize: 14, textAlign: 'right' },
 });
